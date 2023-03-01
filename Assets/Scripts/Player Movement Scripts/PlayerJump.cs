32c17763519e5867d72d1d6887a4b1d17c84a14a
Yanapples <fantasypixel08@gmail.com>
2023-03-01 22:43:20 +0800

    Fixed the stupid bug, Unity is fucking stupid

--- Assets/Scripts/Player Movement Scripts/PlayerJump.cs
+++ Assets/Scripts/Player Movement Scripts/PlayerJump.cs
@@ -109,8 +109,6 @@ public class PlayerJump : MonoBehaviour
         if (groundCheck) Gizmos.DrawWireCube(groundCheck.position, groundCheck.localScale);
     }
 
-
-
     [CustomEditor(typeof(PlayerJump)), CanEditMultipleObjects]
     public class PlayerJumpEditor : Editor
     {
@@ -127,7 +125,8 @@ public class PlayerJump : MonoBehaviour
             if (playerJump.enableGroundChecks)
             {
                 playerJump.groundCheck = EditorGUILayout.ObjectField("Ground Check", playerJump.groundCheck, typeof(Transform)) as Transform;
-                playerJump.groundLayer = EditorGUILayout.LayerField("Ground Layer", playerJump.groundLayer);
+                // playerJump.groundLayer = EditorGUILayout.LayerField("Ground Layer", playerJump.groundLayer);
+                playerJump.groundLayer = LayerMaskField("Ground Layer", playerJump.groundLayer);
             }
 
             playerJump.increaseGWhenFalling = EditorGUILayout.Toggle("Enable Increased Gravity When Falling?", playerJump.increaseGWhenFalling);
@@ -146,6 +145,37 @@ public class PlayerJump : MonoBehaviour
             if (playerJump.enableJumpTimeCooldown)
                 playerJump.jumpInputBuffer = EditorGUILayout.FloatField("Jump Time Cooldown", playerJump.jumpInputBuffer);
 
+        }
+
+        static LayerMask LayerMaskField(string label, LayerMask layerMask)
+        {
+            List<string> layers = new List<string>();
+            List<int> layerNumbers = new List<int>();
+
+            for (int i = 0; i < 32; i++)
+            {
+                string layerName = LayerMask.LayerToName(i);
+                if (layerName != "")
+                {
+                    layers.Add(layerName);
+                    layerNumbers.Add(i);
+                }
+            }
+            int maskWithoutEmpty = 0;
+            for (int i = 0; i < layerNumbers.Count; i++)
+            {
+                if (((1 << layerNumbers[i]) & layerMask.value) > 0)
+                    maskWithoutEmpty |= (1 << i);
+            }
+            maskWithoutEmpty = EditorGUILayout.MaskField(label, maskWithoutEmpty, layers.ToArray());
+            int mask = 0;
+            for (int i = 0; i < layerNumbers.Count; i++)
+            {
+                if ((maskWithoutEmpty & (1 << i)) > 0)
+                    mask |= (1 << layerNumbers[i]);
+            }
+            layerMask.value = mask;
+            return layerMask;
         }
     }
 }
